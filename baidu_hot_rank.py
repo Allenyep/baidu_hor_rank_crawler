c861bfbdcc0fd10f005639df60b089e2f82d90f1
lizili <lizili@kuaishou.com>
2021-07-24 17:38:34 +0800

    add other list

--- baidu_hot_rank.py
+++ baidu_hot_rank.py
@@ -8,27 +8,27 @@ requests.packages.urllib3.disable_warnings()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
 }
-base_url = "https://top.baidu.com/board?tab=realtime"
-response  = requests.get(base_url,headers=headers,verify=False)
-html = response.text
-soup = BeautifulSoup(html)
-a_list = soup.findAll('a',class_='title_dIF3B')
+tab_list = ['realtime','novel','movie','teleplay','cartoon','variety','documentary','star','car','game']
+base_url = "https://top.baidu.com/board?tab="
+
 filename = time.strftime("%Y-%m-%d_%H", time.localtime())
 filedir = time.strftime("%Y-%m-%d", time.localtime())
-
 # 过滤
 re_url = '.*(https.*?)"'
 re_content = '.*sis">(.*?)<'
 
-
 if not os.path.exists('./data/{}'.format(filedir)):
      os.makedirs('./data/{}'.format(filedir))
 
-
-with open('./data/{}/{}.md'.format(filedir, filename),'w+') as f:
-    for idx,obj in enumerate(a_list):
-        content = re.match(re_content,str(obj),re.I).group(1).strip()
-        url = re.match(re_url,str(obj),re.I).group(1)
-        str_news = str(idx+1)+".["+content+"]("+url+")"+"\n"
-        f.write(str_news)
+for i in tab_list:
+    response  = requests.get(base_url + i,headers=headers,verify=False)
+    soup = BeautifulSoup(response.text)
+    a_list = soup.findAll('a',class_='title_dIF3B')
+    with open('./data/{}/{}.md'.format(filedir, filename),'a') as f:
+        f.write("\n" + i + "\n")
+        for idx,obj in enumerate(a_list):
+            content = re.match(re_content,str(obj),re.I).group(1).strip()
+            url = re.match(re_url,str(obj),re.I).group(1)
+            str_news = str(idx+1)+".["+content+"]("+url+")"+"\n"
+            f.write(str_news)
 
